3cc1c13e8a31bc505d9db40bbdbe6334daf8c51b
a0170144@itesm.mx <a01701446@itesm.mx>
2019-09-01 16:59:10 -0500

    Agregar lab6, falta terminar

---
+++ lab6/lab6.js
@@ -0,0 +1,7 @@
+function mouseOver() {
+	document.getElementById("textoCambiante").style.color = "yellow";
+}
+
+function mouseOut() {
+	document.getElementById("textoCambiante").style.color = "black";
+}
