5c4df06d7f93f6c55c3800ee2d8d1d8896fdd132
a0170144@itesm.mx <a01701446@itesm.mx>
2019-09-02 10:41:53 -0500

    avanzar lab6

--- lab6/lab6.js
+++ lab6/lab6.js
@@ -1,7 +1,35 @@
-function mouseOver() {
+function cambiaEstilo() {
 	document.getElementById("textoCambiante").style.color = "yellow";
 }
 
-function mouseOut() {
+function regresaEstilo() {
 	document.getElementById("textoCambiante").style.color = "black";
+}
+
+function infoBlanca() {
+	document.getElementById("blanca").innerHTML = "Blanca: la cinta blanca representa pureza e innocencia.";
+}
+
+function infoAmarilla() {
+	document.getElementById("amarilla").innerHTML = "Amarilla: la cinta amarilla representa una semilla sembrada en la tierra.";
+}
+
+function infoVerde() {
+	document.getElementById("verde").innerHTML = "Verde: la cinta verde representa el primer brote de la semilla.";
+}
+
+function infoAzul() {
+	document.getElementById("azul").innerHTML = "Azul: la cinta azul representa el cielo hacia el cual se dirige el crecimiento de la planta.";
+}
+
+function infoRoja() {
+	document.getElementById("roja").innerHTML = "Roja: la cinta roja representa la flor nacida de la planta madura.";
+}
+
+function infoNegra() {
+	document.getElementById("negra").innerHTML = "Azul: la cinta negra representa peligro, la ausencia de pureza e innocencia y marca el inicio del camino que es el taekwondo como arte marcial.";
+}
+
+function hideSignificado() {
+	setTimeout()
 }
