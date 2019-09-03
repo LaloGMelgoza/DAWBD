0e2809d8cc6c1a8b874d3e4b3c1da37cc723f7c6
a0170144@itesm.mx <a01701446@itesm.mx>
2019-09-03 12:00:55 -0500

    Agregar lab7 terminado

---
+++ lab7/js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+
+    // Init Slider
+
+    $('.slider').slider({ // borrar contenidos
+    	fullWidth: true,
+    	indicators: false,
+    	height: 500
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
